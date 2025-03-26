9c8893778bae87c04985036d16b059472af7eede
Shad <shad.amezng@gmail.com>
2025-03-26 12:19:10 +0530

    added yarn with shadcn

---
+++ hooks/useSiteConfig.js
@@ -0,0 +1,5 @@
+import { siteConfig } from "../site.config.mjs";
+
+export function useSiteConfig() {
+  return siteConfig;
+}
---
+++ site.config.mjs
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  name: 'My Site',
+  description: 'Welcome to my website',
+  url: 'https://mysite.com',
+  // Add any other site-wide configuration values you need
+  social: {
+    twitter: 'https://twitter.com/username',
+    github: 'https://github.com/username',
+  },
+  nav: [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '/about' },
+    { name: 'Blog', href: '/blog' },
+    { name: 'Contact', href: '/contact' },
+  ],
+}
